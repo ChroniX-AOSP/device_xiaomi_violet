81ebccfb28d36e5ff4263ba388e281ea61e3ab1b
Chronix <phoniexpaul88@gmail.com>
2025-09-15 20:23:27 +0530

    Update lineage_violet.mk

--- lineage_violet.mk
+++ lineage_violet.mk
@@ -8,16 +8,12 @@
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
 # Inherit some common DerpFest AOSP stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
-DERPFEST_BUILD_TYPE := Official
+#Cherish
+CHERISH_BUILD_TYPE := UNOFFICIAL
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_NOT_USES_BLUR := false
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_SUPPORTS_CALL_RECORDING := true
-TARGET_INCLUDE_ACCORD ?= true
 
 #GMS
 WITH_GMS := true
@@ -25,16 +21,9 @@ WITH_GAPPS := true
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
-USE_LEGACY_BOOTANIMATION := true
-
-# Charging Animation
-TARGET_INCLUDE_PIXEL_CHARGER := true
 
 # MiuiCamera
 $(call inherit-product-if-exists, vendor/MiuiCamera/config.mk)
-
-# Disable UDFPS animations ( unsupported )
-EXTRA_UDFPS_ICONS := false
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_violet
